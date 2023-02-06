bc65817e7ee05c6914799489a85de68f602f0c6f
505laura <106522200+505laura@users.noreply.github.com>
2023-02-06 06:38:17 +0000

    Add extra routes

--- client/App.js
+++ client/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {ApolloProvider} from '@apollo/client';
 import {ChakraProvider} from '@chakra-ui/react';
 
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 import NoMatch from './pages/NoMatch';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import DashNav from './components/DashNav';
 import Footer from './components/Footer';
 
+import {UserContext} from './utils/userContext';
+
+import {client} from './utils/gqlClient';
+import {config} from './utils/config';
+
 function App() {
+  const [xp, setXP] = React.useState(0);
+  const [level, setLevel] = React.useState(1);
+  const [coins, setCoins] = React.useState(0);
+
+  // Set document title to full name on page load
+  useEffect(() => {
+    document.title = config.siteName;
+  }, []);
+
   return (
     <ChakraProvider>
       <ApolloProvider client={client}>
@@ -20,6 +37,9 @@ function App() {
               <DashNav />
               <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
                 <Route path="*" element={<NoMatch />} />
               </Routes>
               <Footer />
