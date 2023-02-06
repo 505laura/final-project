2624a6442c38bc568d292e8d206ab7771fcc8bd9
505laura <106522200+505laura@users.noreply.github.com>
2023-02-06 06:26:25 +0000

    Add auth utilities

---
+++ server/utils/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SIGNING_SECRET;
+const expiration = '2h';
+
+module.exports = {
+    signToken({username, email, _id}) {
+        const payload = {username, email, _id};
+
+        return jwt.sign({data: payload}, secret, {expiresIn: expiration});
+    },
+    authMiddleware({req}) {
+        // Log the request body
+        console.log(req.body);
+        const tokenInput = req.body.token || req.query.token || req.headers.authorization;
+        let token;
+
+        if(req.headers.authorization) {
+            token = tokenInput.split(' ').pop().trim();
+        }
+
+        if(!token) { return req; }
+
+        try {
+            const {data} = jwt.verify(token, secret, {maxAge: expiration});
+            req.user = data;
+        } catch(e) {
+            console.log('Invalid token');
+        }
+
+        return req;
+    },
+};
