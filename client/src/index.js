d474819b69c91902c4ae81553f18614b49105e6e
505laura <106522200+505laura@users.noreply.github.com>
2023-02-06 06:39:07 +0000

    Move files

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './output.css';
+import './mc_font.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,5 +17,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 // serviceWorker.register();
